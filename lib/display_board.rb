67b21e21cc3626afcfbc5568e69fbb8d6c9343aa
Nicole Parrish <nicoleparrish.parrish@gmail.com>
2019-11-03 19:58:12 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board(board)
+  row1 = " #{board[0]} | #{board[1]} | #{board[2]} "
+  row2 = " #{board[3]} | #{board[4]} | #{board[5]} "
+  row3 = " #{board[6]} | #{board[7]} | #{board[8]} "
+  sep = "-----------"
+  puts row1, sep, row2, sep, row3
+end
